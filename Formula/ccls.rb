70483faef2c45ddad277b807bf3d4c7fe3cabf12
Gong Qijian <gongqijian@gmail.com>
2018-12-17 13:11:46 +0800

    test ccls 0.20181111.1

--- Formula/ccls.rb
+++ Formula/ccls.rb
@@ -7,8 +7,8 @@ class Ccls < Formula
         :revision => "06aa25233567ae16a58a6f5237aacfd9e02aa29d"
   else
     url "https://github.com/MaskRay/ccls.git",
-        :tag => "0.20180924",
-        :revision => "41fcc0272c8c93fcbe52f24ef587e633a9dcc7e1"
+        :tag => "0.20181111.1",
+        :revision => "c2cc5567c0aba467fbe95269bb17fe8a7aa6fe85"
   end
   head "https://github.com/MaskRay/ccls.git"
 
